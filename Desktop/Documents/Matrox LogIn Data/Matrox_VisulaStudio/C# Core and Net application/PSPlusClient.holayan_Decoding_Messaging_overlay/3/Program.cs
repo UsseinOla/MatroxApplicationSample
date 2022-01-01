a4d474e84bc04e0032dc163bd7f5602718242c1b
UsseinOla <77444770+UsseinOla@users.noreply.github.com>
2022-01-01 07:45:50 -0500

    Update Program.cs

--- Desktop/Documents/Matrox LogIn Data/Matrox_VisulaStudio/C# Core and Net application/PSPlusClient.holayan_Decoding_Messaging_overlay/3/Program.cs
+++ Desktop/Documents/Matrox LogIn Data/Matrox_VisulaStudio/C# Core and Net application/PSPlusClient.holayan_Decoding_Messaging_overlay/3/Program.cs
@@ -1,22 +1,3 @@
-﻿// <copyright file="Program.cs" company="Matrox Graphics Inc">
-// (c) [2019-2020], Matrox Graphics Inc. All rights reserved.
-//
-// This software code is subject to the terms and conditions outlined
-// in the non-disclosure agreement between Matrox Graphics Inc. and your company.
-// By accessing this code or using it in any way, you indicate your
-// acceptance of such terms and conditions.
-//
-// All code and information is provided "as is" without warranty of any kind,
-// either expressed or implied, including but not limited to the implied
-// warranties of merchantability and/or fitness for a particular purpose.
-//
-// Disclaimer: Matrox Graphics Inc. reserves the right to make
-// changes in specifications and code at any time and without notice.
-// No responsibility is assumed by Matrox Graphics Inc. for its use;
-// nor for any infringements of patents or other rights of
-// third parties resulting from its use. No license is granted under
-// any patents or patent rights of Matrox Graphics Inc.
-// </copyright>
 
 namespace PSPlusClient.HelloWorld
 {
